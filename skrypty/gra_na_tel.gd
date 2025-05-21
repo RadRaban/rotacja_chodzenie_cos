f87114e7ac868d31c179f01f7d0c831bf3157b6d
RadRaban <rad.raban@gmail.com>
2025-05-21 13:12:52 +0200

    Dobra krzesła działają

--- skrypty/gra_na_tel.gd
+++ skrypty/gra_na_tel.gd
@@ -9,13 +9,20 @@ func _ready():
 		print("Brak obrazka mapy!")
 		return
 
-	var img: Image = mapa_obrazek.get_image()
+	# Pobierz obraz z tekstury
+	var img := mapa_obrazek.get_image()
 
+	# Skonwertuj na nieskompresowany format (np. RGBA8)
+	if img.is_compressed():
+		img.decompress()
+	img.convert(Image.FORMAT_RGBA8)
+
+	# Odczytaj piksele
 	for x in img.get_width():
 		for y in img.get_height():
 			var color = img.get_pixel(x, y)
 
-			# Sprawdzamy czy piksel jest czerwony
+			# Czerwony piksel = wstaw krzesło
 			if color.r > 0.8 and color.g < 0.2 and color.b < 0.2:
 				var pozycja = Vector3(x * rozmiar_kraty, 0, y * rozmiar_kraty)
 				var krzeslo = prefab_krzeslo.instantiate()
